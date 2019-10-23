247482f65b62b55e7079a92e0955061747f98c8c
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-23 01:47:01 -0500

    part 1-7 of tutorial

--- shapes.js
+++ shapes.js
@@ -1,2 +1,49 @@
-var dataArray = [2, 13, 15];
-d3.select("body").append("svg").attr("height", "100%").attr("width", "100%");
+var dataArray = [2, 13, 15, 20, 24, 12, 17];
+
+var svg = 
+  d3.select("body").append("svg")
+    .attr('height', '100%')
+    .attr('width', '100%');
+
+svg.selectAll('rect')
+    .data(dataArray)
+    .enter()
+    .append('rect')
+    .attr('height', function(d, i) {return d*10;})
+    .attr('width', '70')
+    .attr('fill', 'darkred')
+    .attr('x', function(d, i) {return 80*i;})
+    .attr('y', function(d, i) {return 350-(d*10)});
+
+var fixedX=600;
+svg.selectAll('circle.groupa')
+    .data(dataArray)
+    .enter()
+    .append('circle')
+    .attr('circle', 'groupa')
+    .attr('fill', 'blue')
+    .attr('cx', function(d, i){fixedX+=(d*3)+(i*30); return fixedX;})
+    .attr('cy','150')
+    .attr('r', function(d, i) {return d*1.5;})
+
+
+var fixedX=1200;
+svg.selectAll('ellipse')
+    .data(dataArray)
+    .enter()
+    .append('ellipse')
+    .attr('cx', function(d, i){fixedX+=(d*2)+(i*30); return fixedX;})
+    .attr('cy','150')
+    .attr('rx', function(d, i) {return d*1.5;})
+    .attr('ry', '70')
+
+
+var fixedX=0;
+svg.selectAll('line')
+    .data(dataArray)
+    .enter()
+    .append('line')
+    .attr('x1', fixedX)
+    .attr('y1', function(d, i) {return 400 + (i*20);})
+    .attr('x2', function(d) {return fixedX+(d*20);})
+    .attr('y2', function(d, i) {return 400 + (i*20);})
